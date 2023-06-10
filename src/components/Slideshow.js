da064780d9c04428a5d7a2060d1518814870be8f
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-06-10 17:15:23 +0200

    testing some stuff for the slider

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -3,13 +3,45 @@ import arrowRight from "../assets/img/arowRight.png";
 import arrowLeft from "../assets/img/arrowLeft.png";
 import { logement } from "../datas/logements";
 
-const Slideshow = (image) => {
+const Slideshow = (images) => {
   const [counter, setCounter] = useState(0);
   const picture = logement.map(({ pictures }) => [{ picture: { pictures } }]);
   const slideLength = picture.length;
-  console.log(picture);
 
-  return <div></div>;
+  let actualImage;
+  actualImage = picture[counter];
+
+  const slideLeft = () => {
+    setCounter((counter - 1 + slideLength) % slideLength);
+  };
+
+  const slideRight = () => {
+    setCounter((counter + 1) % slideLength);
+  };
+
+  return (
+    <div className="slider-cntr">
+      <div className="slider">
+        <img
+          onClick={slideLeft}
+          className="arrowLeft"
+          src={arrowLeft}
+          alt="fleche de gauche"
+        />
+        <img
+          onClick={slideRight}
+          className="arrowRight"
+          src={arrowRight}
+          alt="fleche de droite"
+        />
+        <img
+          className="picture-appart"
+          src={actualImage}
+          alt="Visualisation de la chambre d'hote"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Slideshow;
