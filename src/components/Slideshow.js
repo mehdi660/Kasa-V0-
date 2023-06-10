a95ec4c2739bdd6d49d24d4d35cd3017874aaee9
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-06-10 17:00:29 +0200

    array created with picture in my data files

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import arrowRight from "../assets/img/arowRight.png";
 import arrowLeft from "../assets/img/arrowLeft.png";
+import { logement } from "../datas/logements";
 
 const Slideshow = (image) => {
   const [counter, setCounter] = useState(0);
+  const picture = logement.map(({ pictures }) => [{ picture: { pictures } }]);
+  const slideLength = picture.length;
+  console.log(picture);
+
   return <div></div>;
 };
 
